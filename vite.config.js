d929d1919ab6fdc6bb9f4483075c0fdd2df22892
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2025-10-02 16:01:28 -0600

    Update vite.config.js
    use server pw_soberania.test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: 'pw_soberania.test',
+        port: 5174,
+        hmr: {
+            host: 'pw_soberania.test',
+        },
+        cors: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
