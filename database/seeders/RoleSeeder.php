ebdeaa9c07d6ce0ab9fc9d1c0da041e4eefff9da
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2025-10-01 20:43:42 -0600

    feat(added user permission roles)
    added seeders:
    - UserSeeder
    - RoleSeeder
    - AssignRoleToUser

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = Role::create([
+            'name' => 'Admin',
+            'description' => 'Administrador',
+        ]);
+
+        $producer = Role::create([
+            'name' => 'Producer',
+            'description' => 'Productor',
+        ]);
+
+        $cooperative = Role::create([
+            'name' => 'Cooperative',
+            'description' => 'Cooperativa',
+        ]);
+
+        $consumer = Role::create([
+            'name' => 'Consumer',
+            'description' => 'Consumidor',
+        ]);
+
+        $permissions = Permission::whereIn('module_key', ['seg', 'menu'])->get();
+        $admin->syncPermissions($permissions);
+    }
+}
