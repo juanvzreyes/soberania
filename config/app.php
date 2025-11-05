d93ccfc715db8d7cfb37cd1269e734c8acd81b84
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2025-11-04 20:46:19 -0600

    feat(lang): add spanish languaje to request validation

--- config/app.php
+++ config/app.php
@@ -77,10 +77,10 @@ return [
     | set to any locale for which you plan to have translation strings.
     |
     */
+    'locale' => 'es',
+    
+    'fallback_locale' => 'es',
 
-    'locale' => env('APP_LOCALE', 'en'),
-
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
